9a409863626a989cbcd99eb7d34cba9c4431c3a5
berkeozdemirr <berke.ozdemir@hiwellapp.com>
2023-10-31 12:12:36 +0300

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as UIToolkit from '@zoom/videosdk-ui-toolkit'
+import '@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'zoomkit';
+}
