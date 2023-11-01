574b0943b1462441eca3aa4569f47b31df656faa
berkeozdemirr <berke.ozdemir@hiwellapp.com>
2023-11-01 10:28:12 +0300

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import * as UIToolkit from '@zoom/videosdk-ui-toolkit'
-import '@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
+}) 
+export class AppComponent implements OnInit, AfterViewInit{
+  ngAfterViewInit(): void {
+  
+    let UIToolKitConfig = {
+      videoSDKJWT: '',
+      sessionName: '',
+      userName: '',
+      sessionPasscode: '',
+      features: ['video', 'audio', 'settings', 'users', 'chat']
+    };
+    
+    UIToolkit.init(UIToolKitConfig);
+    
+  }
+  ngOnInit(): void {
+    let UIKit = document.createElement('app-uitoolkit');
+
+    document.getElementById('UIToolkit')?.append(UIKit);
+
+  }
   title = 'zoomkit';
 }
